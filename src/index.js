a770d08e65404b84ef0429d05fe00460026d393f
Karol Partyka <k.partyka87@gmail.com>
2019-02-22 16:30:37 +0100

    Basic version of map and movement

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ window.onload = () => {
     const appContent = document.getElementById('app');
 
 
-    window.setInterval(() => {
+    var updatePosition = (map, marker) => window.setInterval(() => {
         fetch("http://api.open-notify.org/iss-now.json", {
             mode: "cors",
             credentials: "omit"
@@ -18,7 +18,40 @@ window.onload = () => {
                 issData = data;
                 console.log("asd", data);
                 appContent.innerHTML = `${data.iss_position.latitude} ${data.iss_position.longitude}`;
+
+                moveMarker(map, marker, data.iss_position.latitude, data.iss_position.longitude)
             })
             .catch(error => console.log(error));
     }, 5000);
+
+
+    var initLat = 0;
+    var initLon = 0;
+
+    function initialize() {
+
+        var myLatLng = new google.maps.LatLng(50, 50),
+            myOptions = {
+                zoom: 4,
+                center: myLatLng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(document.getElementById('map-canvas'), myOptions),
+            marker = new google.maps.Marker({ position: myLatLng, map: map });
+
+        marker.setMap(map);
+        // moveMarker(map, marker);
+
+        updatePosition(map, marker);
+
+    }
+
+    function moveMarker(map, marker, lat, lon) {
+        marker.setPosition(new google.maps.LatLng(lat, lon));
+        map.panTo(new google.maps.LatLng(lat, lon));
+    };
+
+    initialize();
+
+
 };
